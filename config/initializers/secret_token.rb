ba440612b48e7a50bd3594a069a1f40ea00aca5e
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2013-09-28 16:04:49 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Essensdates::Application.config.secret_token = '7aaa3db6329fe9865850e0eef2518c38608038201f41233b8a556a359a2c484975e4f28e09ee6ea5012e7793611a898b631d02d742c8f370ed7b0657a6a31d24'
